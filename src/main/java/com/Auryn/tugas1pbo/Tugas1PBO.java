d5faca50fd2f2969c204a4c81449697fe65c4e5e
AurynNajla <aurynn18@gmail.com>
2024-11-17 22:04:48 +0800

    First Commit

---
+++ src/main/java/com/Auryn/tugas1pbo/Tugas1PBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.Auryn.tugas1pbo;
+
+/**
+ *
+ * @author User
+ */
+public class Tugas1PBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
